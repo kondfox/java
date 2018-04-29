6d757089e43ffad9ed31df797c759c78881853a3
kondfox <peter.tamas@greenfoxacademy.com>
2018-04-29 17:48:11 +0200

    feat(Map): create initial map and generate random mines

--- minesweeper/src/Minesweeper.java
+++ minesweeper/src/Minesweeper.java
@@ -1,5 +1,53 @@
+import java.util.Random;
+
 public class Minesweeper {
 
-  
+//    1 2 3 4 5 6
+//  A # # # # # #
+//  B # # # # # #
+//  C # # # # # #
+//  D # # 1 # # #
+
+
+  public static void main(String[] args) {
+
+    int mapWidth = 10;
+    int mapHeight = 10;
+
+    int[][] map = createMap(mapWidth, mapHeight);
+    printMap(map);
+
+  }
+
+  private static void printMap(int[][] map) {
+    for (int i = 0; i < map.length; i++) {
+      for (int j = 0; j < map[i].length; j++) {
+        System.out.print(map[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  private static int[][] createMap(int mapWidth, int mapHeight) {
+    int[][] map = new int[mapHeight][mapWidth];
+    generateMines(map, 10);
+    return map;
+  }
+
+  private static void generateMines(int[][] map, int mineCount) {
+    Random random = new Random();
+    int minesPlanted = 0;
+
+    while (minesPlanted < mineCount) {
+      int randomRowIndex = random.nextInt(map.length);
+      int randomColumnIndex = random.nextInt(map[0].length);
+
+      if (map[randomRowIndex][randomColumnIndex] != 9) {
+        map[randomRowIndex][randomColumnIndex] = 9;
+        minesPlanted++;
+      }
+    }
+
+  }
 
 }
